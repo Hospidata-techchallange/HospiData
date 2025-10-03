b8019f251edc1416b95e54e04a12a4da1cd4accd
Guilherme Ferrari <guilhermesouzaferrari@gmail.com>
2025-10-03 15:10:23 -0300

    finalização parte Gui

---
+++ gateway-service/src/main/java/br/com/hospidata/gateway_service/controller/dto/HistoryRequest.java
@@ -0,0 +1,13 @@
+package br.com.hospidata.gateway_service.controller.dto;
+
+import java.util.UUID;
+
+public record HistoryRequest(
+        UUID patientId,
+        String patientName,
+        String patientEmail,
+        UUID doctorId,
+        String doctorName,
+        String doctorEmail,
+        String description
+) {}
